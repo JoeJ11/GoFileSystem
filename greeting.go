70d04bc3688e7bc5158272ae985c7c34ecfabc34
JoeJ11 <andyjvan@hotmail.com>
2015-05-16 23:34:02 +0800

    Testing on greeting.go

--- greeting.go
+++ greeting.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"bufio"
+	"os"
 )
 
 /*
@@ -14,5 +17,30 @@ func (router GreetingHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) 
 */
 
 func GreetingHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Handle Greeting")
+
+	conn, err := net.Dial("tcp", "localhost:5000")
+	fmt.Println("Connected")
+
+	if (err != nil) {
+		fmt.Println("GreetingHandler::DIAL::Error: " + err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Fprint(conn, "Sending Message\n")
+
+	fmt.Println("Message sent.")
+	connBuf := bufio.NewReader(conn)
+	str, err := connBuf.ReadString('\n')
+	if err != nil {
+		fmt.Println("GreetingHandler::READSTRING::Error: " + err.Error())
+		os.Exit(1)
+	}
+	if len(str) > 0 {
+		fmt.Println(str)
+	}
+
 	fmt.Fprint(w, "Greetings")
+	conn.Close()
+
 }
